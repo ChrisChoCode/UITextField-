56c7a5b69bdfbc5a66ef179596c045f4aa8eb406
CodeWithChris <chrischo490@gmail.com>
2020-11-13 17:47:36 +0800

    Initial Commit

---
+++ UITextField單行文字/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITextField單行文字
+//
+//  Created by Chris Cho on 2020/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
